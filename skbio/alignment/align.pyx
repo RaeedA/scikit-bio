cfaf1d2aaef4cf6f257c039ae40832b6d3dd004c
RaeedA <raeedazom@icloud.com>
2024-12-20 17:22:55 -0700

    adding local alignment as an option

--- skbio/alignment/align.pyx
+++ skbio/alignment/align.pyx
@@ -5,45 +5,81 @@ import numpy as np
 cimport numpy as cnp
 from skbio.alignment import TabularMSA, AlignPath
 from skbio.sequence import DNA
-from libc.stdint cimport int32_t, uint8_t, INT32_MIN
+from libc.stdint cimport int32_t, uint8_t, INT32_MIN, int8_t, int64_t
 cnp.import_array()
+
+#TODO: float support, always output float!
+#TODO: float_t/int_t or float/int?
+#TODO: just add local swap
+#TODO: local alignnment where no negatives in D and traceback from max to 0.
 
 # Scoring constants
 cdef uint8_t GAP = 1
 cdef int32_t NEG_INF = INT32_MIN + 100
 
-def align_wrapper(seq1, seq2, subMatrix, gap_open, gap_extend):
-    seq1_idx = subMatrix._char_hash[DNA(seq1)._bytes]
-    seq2_idx = subMatrix._char_hash[DNA(seq2)._bytes]
-    result, score = align(seq1_idx, seq2_idx, subMatrix._data.astype(int), gap_open, gap_extend)
-    path = AlignPath.from_bits(np.vstack(result))
-    asDNA = [DNA(seq1), DNA(seq2)]
-    return TabularMSA.from_path_seqs(path, asDNA), score
+cdef struct Index:
+    Py_ssize_t i 
+    Py_ssize_t j
 
-cdef align(const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, const cnp.int64_t[:, :] subMatrix, int gap_open, int gap_extend):
+cdef struct TracebackRes:
+    uint8_t score
+    Py_ssize_t idx
+    Index start
+    Index end
+
+def align_wrapper(seq1, seq2, subMatrix, gap_open, gap_extend, scope):
+    #TODO: DNA conversion could cause overhead
+    cdef uint8_t local = scope.lower() == "local"
+    asDNA = [DNA(seq1), DNA(seq2)]
+    seq1_idx = subMatrix._char_hash[asDNA[0]._bytes]
+    seq2_idx = subMatrix._char_hash[asDNA[1]._bytes]
+    max_len = seq1_idx.shape[0] + seq2_idx.shape[0]
+    aligned_seq1 = np.zeros(max_len, dtype=np.uint8)
+    aligned_seq2 = np.zeros(max_len, dtype=np.uint8)
+
+    args = (seq1_idx, seq2_idx, aligned_seq1, aligned_seq2, subMatrix._data.astype(int), gap_open, gap_extend)
+    if(local):
+        args += (local_align_fill, local_align_trace)
+    else:
+        args += (global_align_fill, global_align_trace)
+
+    res = arg_pass(*args)
+
+    result = (aligned_seq1[res['idx']+1:aligned_seq1.size], aligned_seq2[res['idx']+1:aligned_seq2.size])
+    path = AlignPath.from_bits(np.vstack(result))
+    if(local):
+        asDNA = [DNA(seq1[res['start']['i']:res['end']['i']]), DNA(seq2[res['start']['j']:res['end']['j']])]
+    return TabularMSA.from_path_seqs(path, asDNA), res['score']
+
+def arg_pass(const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, cnp.ndarray aligned_seq1, cnp.ndarray aligned_seq2, const cnp.int64_t[:, :] subMatrix, const int8_t gap_open, const int8_t gap_extend, fill_func, trace_func):
+    return align_main(seq1, seq2, aligned_seq1, aligned_seq2, subMatrix, gap_open, gap_extend, fill_func, trace_func)
+
+cdef TracebackRes align_main(const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, cnp.ndarray aligned_seq1, cnp.ndarray aligned_seq2, const cnp.int64_t[:, :] subMatrix, const int8_t gap_open, const int8_t gap_extend, fill_func, trace_func) noexcept:
     cdef Py_ssize_t m = seq1.shape[0]
     cdef Py_ssize_t n = seq2.shape[0]
-    cdef int score
-    cdef uint8_t current_matrix
+    cdef Py_ssize_t idx
+    cdef TracebackRes res
+    cdef uint8_t score
     cdef Py_ssize_t max_len = m + n
     cdef D = np.empty((m + 1, n + 1), dtype=np.int32)
     cdef P = np.empty((m + 1, n + 1), dtype=np.int32)
     cdef Q = np.empty((m + 1, n + 1), dtype=np.int32)
-    cdef cnp.ndarray[cnp.uint8_t, ndim=1] aligned_seq1 = np.zeros(max_len, dtype=np.uint8)
-    cdef cnp.ndarray[cnp.uint8_t, ndim=1] aligned_seq2 = np.zeros(max_len, dtype=np.uint8)
 
     cdef int32_t[:, :] D_view = D
     cdef int32_t[:, :] P_view = P
     cdef int32_t[:, :] Q_view = Q
 
-    align_fill(D_view, P_view, Q_view, subMatrix, seq1, seq2, gap_open, gap_extend)
+    cdef uint8_t[::1] aligned_seq1_view = aligned_seq1
+    cdef uint8_t[::1] aligned_seq2_view = aligned_seq2
 
-    cdef Py_ssize_t idx = align_trace(D_view, P_view, Q_view, m, n, max_len, aligned_seq1, aligned_seq2)
+    cdef Index loc = Index(i = m, j = n)
 
-    return (aligned_seq1[idx+1:aligned_seq1.size], aligned_seq2[idx+1:aligned_seq2.size]), max(D_view[m, n], P_view[m, n], Q_view[m, n])
+    fill_func(D_view, P_view, Q_view, subMatrix, seq1, seq2, gap_open, gap_extend)
+    res = trace_func(D_view, P_view, Q_view, loc, max_len, aligned_seq1_view, aligned_seq2_view)
+    return res
 
-cdef void align_fill(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, const cnp.int64_t[:, :] subMatrix, const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, const int GAP_OPEN_PENALTY, const int GAP_EXTEND_PENALTY) noexcept nogil:
-    cdef Py_ssize_t i, j, idx
+cdef void global_align_fill(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, const cnp.int64_t[:, :] subMatrix, const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, const int8_t GAP_OPEN_PENALTY, const int8_t GAP_EXTEND_PENALTY) noexcept nogil:
+    cdef Py_ssize_t i, j
     cdef Py_ssize_t m = seq1.shape[0]
     cdef Py_ssize_t n = seq2.shape[0]
 
@@ -51,23 +87,30 @@ cdef void align_fill(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q
     P_view[0, 0] = NEG_INF
     Q_view[0, 0] = NEG_INF
 
+    #TODO: look at numpy arrange/line space to fill the first row/col for D, Q, and P (even spacing)
+
     for i in range(1, m + 1):
         D_view[i, 0] = GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY * (i-1)
         Q_view[i, 0] = NEG_INF
+
+    #TODO: can set i,0 to 0,j using numpy to improve performance more!
 
     for j in range(1, n + 1):
         D_view[0, j] = GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY * (j-1)
         P_view[0, j] = NEG_INF
 
+    #TODO: split it here, initial fill vs rest of fill
+    #TODO: look into numpy.nditer for iteration through the matrix
+
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             P_view[i, j] = max(
-                D_view[i - 1, j] + GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY * 0,
+                D_view[i - 1, j] + GAP_OPEN_PENALTY,
                 P_view[i - 1, j] + GAP_EXTEND_PENALTY
             )
 
             Q_view[i, j] = max(
-                D_view[i, j - 1] + GAP_OPEN_PENALTY + GAP_EXTEND_PENALTY * 0,
+                D_view[i, j - 1] + GAP_OPEN_PENALTY,
                 Q_view[i, j - 1] + GAP_EXTEND_PENALTY
             )
 
@@ -77,50 +120,165 @@ cdef void align_fill(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q
                 Q_view[i, j]
             )
 
-cdef int align_trace(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, Py_ssize_t m, Py_ssize_t n, Py_ssize_t max_len, aligned_seq1, aligned_seq2) noexcept :
+cdef void local_align_fill(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, const cnp.int64_t[:, :] subMatrix, const cnp.uint8_t[::1] seq1, const cnp.uint8_t[::1] seq2, const int8_t GAP_OPEN_PENALTY, const int8_t GAP_EXTEND_PENALTY) noexcept nogil:
     cdef Py_ssize_t i, j, idx
-    cdef uint8_t[::1] aligned_seq1_view = aligned_seq1
-    cdef uint8_t[::1] aligned_seq2_view = aligned_seq2
+    cdef Py_ssize_t m = seq1.shape[0]
+    cdef Py_ssize_t n = seq2.shape[0]
 
-    if D_view[m, n] >= P_view[m, n] and D_view[m, n] >= Q_view[m, n]:
+    D_view[0, 0] = 0
+    P_view[0, 0] = NEG_INF
+    Q_view[0, 0] = NEG_INF
+
+    #TODO: look at numpy arrange/line space to fill the first row/col for D, Q, and P (even spacing)
+
+    for i in range(1, m + 1):
+        D_view[i, 0] = 0
+        Q_view[i, 0] = NEG_INF
+        P_view[i, 0] = 0
+
+    #TODO: can set i,0 to 0,j using numpy to improve performance more!
+
+    for j in range(1, n + 1):
+        D_view[0, j] = 0
+        P_view[0, j] = NEG_INF
+        Q_view[0, j] = 0
+
+    #TODO: split it here, initial fill vs rest of fill
+    #TODO: look into numpy.nditer for iteration through the matrix
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            P_view[i, j] = max(
+                D_view[i - 1, j] + GAP_OPEN_PENALTY,
+                P_view[i - 1, j] + GAP_EXTEND_PENALTY
+            )
+
+            Q_view[i, j] = max(
+                D_view[i, j - 1] + GAP_OPEN_PENALTY,
+                Q_view[i, j - 1] + GAP_EXTEND_PENALTY
+            )
+
+            D_view[i, j] = max(
+                D_view[i - 1, j - 1] + subMatrix[seq1[i-1], seq2[j-1]],
+                P_view[i, j],
+                Q_view[i, j],
+                0
+            )
+
+cdef TracebackRes global_align_trace(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, Index loc, Py_ssize_t max_len, uint8_t[::1] aligned_seq1_view, uint8_t[::1] aligned_seq2_view) noexcept nogil:
+    cdef uint8_t current_matrix, score
+    cdef TracebackRes res
+    cdef Py_ssize_t idx = max_len - 1
+
+    if D_view[loc.i, loc.j] >= P_view[loc.i, loc.j] and D_view[loc.i, loc.j] >= Q_view[loc.i, loc.j]:
+        score = D_view[loc.i, loc.j]
         current_matrix = 0
-    elif P_view[m, n] >= Q_view[m, n]:
+    elif P_view[loc.i, loc.j] >= Q_view[loc.i, loc.j]:
+        score = P_view[loc.i, loc.j]
         current_matrix = 1
     else:
+        score = Q_view[loc.i, loc.j]
         current_matrix = 2
 
-    # Traceback
-    i = m
-    j = n
-    idx = max_len - 1
-
-    while i > 0 or j > 0:
-        if current_matrix == 0 and D_view[i-1][j-1] >= P_view[i][j] and D_view[i-1][j-1] >= Q_view[i][j]:
-            i -= 1
-            j -= 1
-        elif (current_matrix == 0 and P_view[i][j] >= Q_view[i][j]) or current_matrix == 1:
+    while loc.i > 0 or loc.j > 0:
+        #TODO: store them in var if needed
+        if current_matrix == 0:
+            if D_view[loc.i-1, loc.j-1] >= P_view[loc.i, loc.j] and D_view[loc.i-1, loc.j-1] >= Q_view[loc.i, loc.j]:
+                loc.i -= 1
+                loc.j -= 1
+            elif P_view[loc.i, loc.j] >= Q_view[loc.i, loc.j]:
+                aligned_seq2_view[idx] = GAP
+                if D_view[loc.i-1, loc.j] <= P_view[loc.i-1, loc.j]:
+                    current_matrix = 1
+                loc.i -= 1
+            else:
+                aligned_seq1_view[idx] = GAP
+                if D_view[loc.i, loc.j-1] <= Q_view[loc.i, loc.j-1]:
+                    current_matrix = 2
+                loc.j -= 1
+        elif current_matrix == 1:
             aligned_seq2_view[idx] = GAP
-            if D_view[i-1][j] > P_view[i-1][j]:
+            if D_view[loc.i-1, loc.j] > P_view[loc.i-1, loc.j]:
                 current_matrix = 0
-            else:
-                current_matrix = 1
-            i -= 1
-        elif (current_matrix == 0 and Q_view[i][j] > P_view[i][j]) or current_matrix == 2:
+            loc.i -= 1
+        else:
             aligned_seq1_view[idx] = GAP
-            if D_view[i][j-1] > Q_view[i][j-1]:
-                current_matrix = 0
-            else:
-                current_matrix = 2    
-            j -= 1
+            if D_view[loc.i, loc.j-1] > Q_view[loc.i, loc.j-1]:
+                current_matrix = 0 
+            loc.j -= 1
         idx -= 1
         
-    while i > 0:
+    while loc.i > 0:
         aligned_seq2_view[idx] = GAP
-        i -= 1
+        loc.i -= 1
         idx -= 1
-    while j > 0:
+    while loc.j > 0:
         aligned_seq1_view[idx] = GAP
-        j -= 1
+        loc.j -= 1
+        idx -= 1
+    
+    res.score = score
+    res.idx = idx
+    return res
+
+cdef TracebackRes local_align_trace(int32_t[:, :] D_view, int32_t[:, :] P_view, int32_t[:, :] Q_view, Index loc, Py_ssize_t max_len, uint8_t[::1] aligned_seq1_view, uint8_t[::1] aligned_seq2_view) noexcept:
+    cdef Index P_loc, D_loc, Q_loc, end
+    cdef uint8_t current_matrix, score
+    cdef TracebackRes res
+    cdef Py_ssize_t idx = max_len - 1
+    
+    P_loc.i, P_loc.j = np.unravel_index(np.argmax(P_view), np.shape(P_view))
+    D_loc.i, D_loc.j = np.unravel_index(np.argmax(D_view), np.shape(D_view))
+    Q_loc.i, Q_loc.j = np.unravel_index(np.argmax(Q_view), np.shape(Q_view))
+
+    if D_view[D_loc.i, D_loc.j] >= P_view[P_loc.i, P_loc.j] and D_view[D_loc.i, D_loc.j] >= Q_view[Q_loc.i, Q_loc.j]:
+        current_matrix = 0
+        score = D_view[D_loc.i, D_loc.j]
+        loc = D_loc
+    elif P_view[P_loc.i, P_loc.j] >= Q_view[Q_loc.i, Q_loc.j]:
+        current_matrix = 1
+        score = P_view[P_loc.i, P_loc.j]
+        loc = P_loc
+    else:
+        current_matrix = 2
+        score = Q_view[Q_loc.i, Q_loc.j]
+        loc = Q_loc
+    
+    end = loc
+
+    while loc.i > 0 or loc.j > 0:
+        #TODO: outside if/elif/else just for comparing current_matrix, then compare P_Q_D inside and store them in var if needed
+        if current_matrix == 0:
+            if D_view[loc.i-1, loc.j-1] >= P_view[loc.i, loc.j] and D_view[loc.i-1, loc.j-1] >= Q_view[loc.i, loc.j]:
+                loc.i -= 1
+                loc.j -= 1
+            elif P_view[loc.i, loc.j] >= Q_view[loc.i, loc.j]:
+                aligned_seq2_view[idx] = GAP
+                if D_view[loc.i-1, loc.j] <= P_view[loc.i-1, loc.j]:
+                    current_matrix = 1
+                loc.i -= 1
+            else:
+                aligned_seq1_view[idx] = GAP
+                if D_view[loc.i, loc.j-1] <= Q_view[loc.i, loc.j-1]:
+                    current_matrix = 2
+                loc.j -= 1
+            if D_view[loc.i, loc.j] == 0:
+                idx -= 1
+                break
+        elif current_matrix == 1:
+            aligned_seq2_view[idx] = GAP
+            if D_view[loc.i-1, loc.j] > P_view[loc.i-1, loc.j]:
+                current_matrix = 0
+            loc.i -= 1
+        else:
+            aligned_seq1_view[idx] = GAP
+            if D_view[loc.i, loc.j-1] > Q_view[loc.i, loc.j-1]:
+                current_matrix = 0 
+            loc.j -= 1
         idx -= 1
 
-    return idx
+    res.score = score
+    res.idx = idx
+    res.start = loc
+    res.end = end
+    return res
--- skbio/alignment/runAlign.py
+++ skbio/alignment/runAlign.py
@@ -1,13 +1,8 @@
 import timeit
-from skbio.sequence import DNA
-from skbio.alignment import global_pairwise_align
+from align import align_wrapper
 from skbio.sequence import SubstitutionMatrix as SubMatrix
-import numpy as np
-from Bio.Align import substitution_matrices, Alignment
-from biotite.sequence import NucleotideSequence
+from Bio.Align import substitution_matrices
 from Bio import Align
-from biotite.sequence.align import align_optimal, SubstitutionMatrix
-from skbio.alignment.align import align_wrapper
 
 
 def run(seq1, seq2):
@@ -16,28 +11,23 @@ def run(seq1, seq2):
     seq1 = seq1.upper()
     seq2 = seq2.upper()
     submat = SubMatrix.by_name("NUC.4.4")
+    scope = "global"
 
-    res, score = align_wrapper(seq1, seq2, submat, -gap_open, -gap_extend)
+    res, score = align_wrapper(seq1, seq2, submat, -gap_open, -gap_extend, scope)
     print("Mine:", score)
+    print(res)
     aligner = Align.PairwiseAligner(
         substitution_matrix=substitution_matrices.load("NUC.4.4"),
         open_gap_score=-gap_open,
         extend_gap_score=-gap_extend,
+        mode=scope,
     ).align(seq1, seq2)
     print("BioPy:", aligner[0].score)
+    print(aligner[0])
     # test = global_pairwise_align(DNA(seq1),
     #       DNA(seq2), gap_open, gap_extend, submat.to_dict())
     # print("SciKit:", test[1])
     # print(test[0])
-    print(
-        "Biotite:",
-        align_optimal(
-            NucleotideSequence(seq1),
-            NucleotideSequence(seq2),
-            SubstitutionMatrix.std_nucleotide_matrix(),
-            (-gap_open, -gap_extend),
-        )[0].score,
-    )
 
 
 def times(seq1, seq2):
@@ -46,8 +36,8 @@ def times(seq1, seq2):
     seq1 = seq1.upper()
     seq2 = seq2.upper()
     submat = SubMatrix.by_name("NUC.4.4")
-    num = 100
-    print("Test with 100")
+    num = 1
+    print("Test with 1")
     result = timeit.Timer(
         lambda: align_wrapper(seq1, seq2, submat, -gap_open, -gap_extend)
     ).timeit(num)
@@ -60,41 +50,28 @@ def times(seq1, seq2):
         ).align(seq1, seq2)
     ).timeit(num)
     print(f"biopy: {result/num:.5f}s")
-    result = timeit.Timer(
-        lambda: align_optimal(
-            NucleotideSequence(seq1),
-            NucleotideSequence(seq2),
-            SubstitutionMatrix.std_nucleotide_matrix(),
-            (-gap_open, -gap_extend),
-        )
-    ).timeit(num)
-    print(f"biotite: {result/num:.5f}s")
-    num = 10000
-    print("Test with 10000")
-    result = timeit.Timer(
-        lambda: align_wrapper(seq1, seq2, submat, -gap_open, -gap_extend)
-    ).timeit(num)
-    print(f"mine: {result/num:.5f}s")
-    result = timeit.Timer(
-        lambda: Align.PairwiseAligner(
-            substitution_matrix=substitution_matrices.load("NUC.4.4"),
-            open_gap_score=-gap_open,
-            extend_gap_score=-gap_extend,
-        ).align(seq1, seq2)
-    ).timeit(num)
-    print(f"biopy: {result/num:.5f}s")
+    # num = 10000
+    # print("Test with 10000")
+    # result = timeit.Timer(
+    #     lambda: align_wrapper(seq1, seq2, submat, -gap_open, -gap_extend)
+    # ).timeit(num)
+    # print(f"mine: {result/num:.5f}s")
+    # result = timeit.Timer(
+    #     lambda: Align.PairwiseAligner(
+    #         substitution_matrix=substitution_matrices.load("NUC.4.4"),
+    #         open_gap_score=-gap_open,
+    #         extend_gap_score=-gap_extend,
+    #     ).align(seq1, seq2)
+    # ).timeit(num)
+    # print(f"biopy: {result/num:.5f}s")
 
 
 if __name__ == "__main__":
     run(
-        "gcgcgtgcgcggaaggagccaaggtgaagttgtagcagtgtgtcagaagaggtgcgtggcaccatgctgtcccccgaggcggagcgggtgctgcggtacctggtcgaagtagaggagttgg",
-        "gacttgtggaacctacttcctgaaaataaccttctgtcctccgagctctccgcacccgtggatgacctgctcccgtacacagatgttgccacctggctggatgaatgtccgaatgaagcg",
-    )
-    times(
-        "gcgcgtgcgcggaaggagccaaggtgaagttgtagcagtgtgtcagaagaggtgcgtggcaccatgctgtcccccgaggcggagcgggtgctgcggtacctggtcgaagtagaggagttg",
-        "gacttgtggaacctacttcctgaaaataaccttctgtcctccgagctctccgcacccgtggatgacctgctcccgtacacagatgttgccacctggctggatgaatgtccgaatgaagcg",
+        "CCGTTA",
+        "CCGATGTT",
     )
     # with open('skbio/alignment/a.txt', 'r') as a,
-    #      open('skbio/alignment/a.txt', 'r') as b:
-    #     times(a.read(), b.read())
+    # open('skbio/alignment/a.txt', 'r') as b:
+    #     run(a.read(), b.read())
     # run('gcgt', 'gcgtt')
